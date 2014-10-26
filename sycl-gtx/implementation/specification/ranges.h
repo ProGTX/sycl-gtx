a0c141fc7a41e88bfd1941aee0f51ff34a0a9c19
Peter Žužek <peterzuzek@gmail.com>
2014-10-26 22:24:35 +0100

    Started implementing ranges and identifiers.

---
+++ sycl-gtx/implementation/specification/ranges.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <array>
+
+namespace cl {
+namespace sycl {
+
+// 3.4.1 Ranges and identifiers
+
+namespace helper {
+
+} // namespace helper
+
+template <int dims>
+class range;
+
+template <int dims>
+class nd_range;
+
+} // namespace sycl
+} // namespace cl
--- sycl-gtx/implementation/sycl.hpp
+++ sycl-gtx/implementation/sycl.hpp
@@ -1,1 +1,13 @@
 #pragma once
+
+// TODO
+#define CL_SYCL_LANGUAGE_VERSION
+
+#include "specification\ranges.h"
+
+namespace cl {
+namespace sycl {
+
+
+} // namespace sycl
+} // namespace cl
