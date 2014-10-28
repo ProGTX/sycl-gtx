466b0ce164dbee63a00adbcea3dcbad5fbd41bb9
Peter Žužek <peterzuzek@gmail.com>
2014-10-28 21:30:28 +0100

    Merged device.h with device_selector.h.

--- sycl-gtx/implementation/specification/device.h
+++ sycl-gtx/implementation/specification/device.h
@@ -1,12 +1,27 @@
 #pragma once
 
-// 3.2.2 Device class
+// Device classes
 
 namespace cl {
 namespace sycl {
 
+// 3.2.2 Device class
 // Encapsulates a cl_device_id and a cl_platform_id
 class device {};
+
+
+// 3.2.4 Device selection class
+// The class device_selector is an abstract class which enables the SYCL runtime to choose the best device based
+// on heuristics specified by the user, or by one of the built-in device selectors
+class device_selector {
+public:
+	virtual int operator()(device dev) = 0;
+};
+
+// Built-in device selectors:
+// Class name: gpu_selector
+// Class name: cpu_selector
+// Class name: host_selector
 
 } // namespace sycl
 } // namespace cl
