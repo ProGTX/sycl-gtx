df86d82be60e8a5901bd0e6bad87c7f30b8a8165
Peter Žužek <peterzuzek@gmail.com>
2014-10-28 11:18:02 +0100

    Expanded accessors.

--- sycl-gtx/implementation/specification/accessors.h
+++ sycl-gtx/implementation/specification/accessors.h
@@ -16,7 +16,24 @@ enum mode {
 	discard_read_write,	// read/write access, previous target object contents discarded
 };
 
+// 3.3.4.2 Access targets
+enum target {
+	global_buffer,		// access buffer via __global memory
+	constant_buffer,	// access buffer via __constant memory
+	local,				// access work - group - local memory
+	image,				// access an image
+	host_buffer,		// access buffer immediately on the host
+	host_image,			// access image immediately on the host
+	image_array,		// access an array of images on device
+	cl_buffer,			// access an OpenCL cl_mem buffer on device
+	cl_image,			// access an OpenCL cl_mem image on device
+};
+
 } // namespace access
+
+// 3.3.4.4 Buffer accessors
+template <typename dataType, int dimensions, access::mode mode, access::target target>
+class accessor {};
 
 } // namespace sycl
 } // namespace cl
