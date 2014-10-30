8efd10e9e4a2704ee84016d848a1f655b2ea4686
Peter Žužek <peterzuzek@gmail.com>
2014-10-30 20:48:40 +0100

    Improved test5 to a basic test of reference counting.

--- sycl-gtx/tests/test5.cpp
+++ sycl-gtx/tests/test5.cpp
@@ -1,5 +1,7 @@
 #include "tests.h"
 
+#include "../implementation/specification/refc.h"
+
 bool test5() {
 	static const int MAX_PLATFORMS = 10;
 	static const int BUFFER_SIZE = 1024;
@@ -18,9 +20,9 @@ bool test5() {
 	check_error(clGetPlatformIDs(MAX_PLATFORMS, platforms, &num_platforms));
 
 	debug() << "num_platforms:" << num_platforms;
-	debug();
 
 	for(int i = 0; i < num_platforms; ++i) {
+		debug();
 		debug() << "platform num:\t" << i;
 		check_error(clGetPlatformInfo(platforms[i], CL_PLATFORM_PROFILE, BUFFER_SIZE, buffer, NULL));
 		debug() << "profile:\t" << buffer;
@@ -32,7 +34,47 @@ bool test5() {
 		debug() << "vendor:\t\t" << buffer;
 		check_error(clGetPlatformInfo(platforms[i], CL_PLATFORM_EXTENSIONS, BUFFER_SIZE, buffer, NULL));
 		debug() << "extensions:\t" << buffer;
+	}
+
+	if(success && num_platforms > 0) {
 		debug();
+#define USE_SYCL 1
+#if USE_SYCL
+		using namespace cl::sycl;
+
+		auto platform = refc::allocate<cl_platform_id>();
+		auto platform_id = platform.get();
+		cl_uint num_devices;
+		auto device = refc::allocate<cl_device_id>();
+		auto device_id = device.get();
+		cl_int ret;
+
+		debug() << "getting platform";
+		check_error(clGetPlatformIDs(1, &platform_id, &num_platforms));
+		debug() << "getting device";
+		check_error(clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &num_devices));
+		debug() << "creating context";
+		auto context = refc::allocate(clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret), clReleaseContext);
+		check_error(ret);
+#else
+		cl_context context = NULL;
+		cl_device_id device_id = NULL;
+		cl_platform_id platform_id = NULL;
+		cl_uint num_devices;
+		cl_int ret;
+
+		debug() << "getting platform";
+		check_error(clGetPlatformIDs(1, &platform_id, &num_platforms));
+		debug() << "getting device";
+		check_error(clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &num_devices));
+		debug() << "creating context";
+		context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
+
+		debug() << "releasing context";
+		check_error(clReleaseContext(context));
+		debug() << "releasing device";
+		check_error(clReleaseDevice(device_id));
+#endif
 	}
 
 	return success;
