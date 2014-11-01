33ae01a0378ac41c0229fced61ff85f2b2f4b818
Peter Žužek <peterzuzek@gmail.com>
2014-11-01 13:11:32 +0100

    Started implementing proper error handling.

--- sycl-gtx/implementation/specification/error_handler.h
+++ sycl-gtx/implementation/specification/error_handler.h
@@ -1,9 +1,86 @@
 #pragma once
 
+// 3.5 Error handling
+
 #include "../common.h"
+#include "../debug.h"
 
 namespace cl {
 namespace sycl {
+
+// Forward declarations
+class queue;
+template <typename T, int dimensions>
+struct buffer;
+class image;
+
+class exception {
+private:
+	cl_int error_code;
+	bool is_sycl_specific;
+
+	void* thrower;
+	enum class thrower_t {
+		queue, buffer, image
+	};
+	thrower_t thrower_type;
+
+	template<class T, thrower_t required_type>
+	T* get() {
+		return (thrower_type == required_type ? reinterpret_cast<T*>(thrower) : nullptr);
+	}
+
+public:
+	template <class T>
+	exception(T* thrower, cl_int error_code, bool is_sycl_specific = false);
+
+	template <>
+	exception(queue* thrower, cl_int error_code, bool is_sycl_specific)
+		: thrower(thrower), error_code(error_code), is_sycl_specific(is_sycl_specific), thrower_type(thrower_t::queue) {}
+
+	// TODO: Handling of buffer template parameters
+	//template <>
+	//exception(buffer* thrower, cl_int error_code, bool is_sycl_specific)
+	//	: thrower(thrower), error_code(error_code), is_sycl_specific(is_sycl_specific), thrower_type(thrower_t::buffer) {}
+
+	template <>
+	exception(image* thrower, cl_int error_code, bool is_sycl_specific)
+		: thrower(thrower), error_code(error_code), is_sycl_specific(is_sycl_specific), thrower_type(thrower_t::image) {}
+
+	// Returns the OpenCL error code.
+	// Returns 0 if not an OpenCL error
+	cl_int get_cl_code() {
+		return (is_sycl_specific ? 0 : error_code);
+	}
+
+	// Returns the SYCL-specific error code.
+	// Returns 0 if not a SYCL-specific error
+	cl_int get_sycl_code() {
+		return (is_sycl_specific ? error_code : 0);
+	}
+
+	// Returns the queue that caused the error.
+	// Returns 0 if not a queue error
+	queue* get_queue() {
+		return get<queue, thrower_t::queue>();
+	}
+
+	// Returns the buffer that caused the error.
+	// Returns 0 if not a buffer error
+	//buffer* get_buffer();
+
+	// Returns the image that caused the error.
+	// Returns 0 if not a image error
+	image* get_image() {
+		return get<image, thrower_t::image>();
+	}
+};
+
+class error_handler {
+public:
+	//  called on error
+	virtual void report_error(exception& error) = 0;
+};
 
 namespace helper {
 
