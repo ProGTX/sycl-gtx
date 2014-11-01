9d1eb67fe89b3eb967a1cefa707b4933095a405d
Peter Žužek <peterzuzek@gmail.com>
2014-11-01 22:52:28 +0100

    Added error handling to platform class.

--- sycl-gtx/implementation/specification/platform.cpp
+++ sycl-gtx/implementation/specification/platform.cpp
@@ -7,18 +7,41 @@
 using namespace cl::sycl;
 
 platform::platform(cl_platform_id platform_id)
-	: platform_id(refc::allocate(platform_id)) {}
+	: platform(platform_id, helper::error::handler::default) {}
+
+platform::platform(error_handler& handler)
+	: platform(nullptr, handler) {}
+
+platform::platform(int& error_code)
+	: platform(nullptr, error_code) {}
+
+platform::platform(cl_platform_id platform_id, error_handler& handler)
+	: platform_id(refc::allocate(platform_id)), handler(handler) {}
+
+platform::platform(cl_platform_id platform_id, int& error_code)
+	: platform_id(refc::allocate(platform_id)), handler(error_code) {}
 
 cl_platform_id platform::get() const {
 	return platform_id.get();
 }
 
 VECTOR_CLASS<platform> platform::get_platforms() {
+	return get_platforms(helper::error::handler());
+}
+VECTOR_CLASS<platform> platform::get_platforms(int& error_code) {
+	return get_platforms(helper::error::handler(error_code));
+}
+VECTOR_CLASS<platform> platform::get_platforms(error_handler& handler) {
+	return get_platforms(helper::error::handler(handler));
+}
+
+VECTOR_CLASS<platform> platform::get_platforms(helper::error::handler& handler) {
 	static const int MAX_PLATFORMS = 1024;
-	cl_platform_id platforms_ids[MAX_PLATFORMS];
+	cl_platform_id platform_ids[MAX_PLATFORMS];
 	cl_uint num_platforms;
-	auto error_code = clGetPlatformIDs(MAX_PLATFORMS, platforms_ids, &num_platforms);
-	return helper::to_vector<platform>(platforms_ids, num_platforms);
+	auto error_code = clGetPlatformIDs(MAX_PLATFORMS, platform_ids, &num_platforms);
+	handler.report(platform_ids, error_code);
+	return helper::to_vector<platform>(platform_ids, num_platforms);
 }
 
 VECTOR_CLASS<device> platform::get_devices(cl_device_type device_type) {
--- sycl-gtx/implementation/specification/platform.h
+++ sycl-gtx/implementation/specification/platform.h
@@ -16,16 +16,26 @@ class device;
 class platform {
 private:
 	refc::ptr<cl_platform_id> platform_id;
+	helper::error::handler handler;
 
 public:
 	platform(cl_platform_id platform_id = nullptr);
+	platform(error_handler& handler);
+	platform(int& error_code);
+	platform(cl_platform_id platform_id, error_handler& handler);
+	platform(cl_platform_id platform_id, int& error_code);
 
 	cl_platform_id get() const;
 
 	// Returns a vector of platforms.
 	// Errors can be returned via C++ exceptions or via a reference to an error_code.
 	static VECTOR_CLASS<platform> get_platforms();
-	
+	static VECTOR_CLASS<platform> get_platforms(error_handler& handler);
+	static VECTOR_CLASS<platform> get_platforms(int& error_code);
+private:
+	static VECTOR_CLASS<platform> get_platforms(helper::error::handler& handler);
+
+public:
 	// TODO: There's probably an error in the specification - get_devices cannot be overloaded on "static" alone.
 
 	// Returns a vector of corresponding devices.
@@ -38,6 +48,7 @@ public:
 		char buffer[BUFFER_SIZE];
 		auto pid = platform_id.get();
 		auto error_code = clGetPlatformInfo(pid, name, BUFFER_SIZE, buffer, nullptr);
+		handler.report(this, error_code);
 		return buffer;
 	}
 
