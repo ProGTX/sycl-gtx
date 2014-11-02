72a702a39af52ba1b825f5250fa07e3830c64cad
Peter Žužek <peterzuzek@gmail.com>
2014-11-02 12:28:45 +0100

    Added error handling constructors to context class.

--- sycl-gtx/implementation/specification/context.cpp
+++ sycl-gtx/implementation/specification/context.cpp
@@ -1,6 +1,19 @@
 #include "context.h"
+#include "device.h"
 
 using namespace cl::sycl;
+
+error_handler& context::default_error = helper::error::handler::default;
+
+// Error handling via error_handler&
+context::context(cl_context c, error_handler& handler)
+	: ctx(refc::allocate<cl_context>(c, clReleaseContext)) {
+	// TODO: In the case of copying the context it calls a clRetainContext
+}
+context::context(device_selector& dev_sel, error_handler& handler) {}
+context::context(const cl_context_properties* properties, device_selector& dev_sel, error_handler& handler) {}
+context::context(const cl_context_properties* properties, VECTOR_CLASS<device> target_devices, error_handler& handler) {}
+context::context(const cl_context_properties* properties, device target_device, error_handler& handler) {}
 
 cl_context context::get() {
 	return ctx.get();
--- sycl-gtx/implementation/specification/context.h
+++ sycl-gtx/implementation/specification/context.h
@@ -14,22 +14,37 @@ class device;
 class device_selector;
 class program;
 
+// Used as the notification function for contexts.
+class context_notify {
+public:
+	virtual void operator()(const STRING_CLASS errinfo, const void* private_info, size_t cb) = 0;
+	virtual void operator()(program t_program) = 0;
+};
+
 class context {
 private:
 	refc::ptr<cl_context> ctx;
+	helper::error::handler handler;
+	static error_handler& default_error;
 
 public:
 	// TODO: The constructor creates a context and in the case of copying it calls a clRetainContext
-	context() {
-		DSELF() << "not implemented";
-	}
-	context(device_selector dev_sel);
-	context(const cl_context_properties* properties, device_selector dev_sel);
-	context(const cl_context_properties* properties, VECTOR_CLASS<device> target_devices);
-	context(const cl_context_properties* properties, device target_device);
-	context(cl_context c);
 
-	// TODO: On destruction clReleaseContext is called.
+	// TODO: Error handling via error_handler&
+	context(cl_context c = nullptr, error_handler& handler = default_error);
+	context(device_selector& dev_sel, error_handler& handler = default_error);
+	context(const cl_context_properties* properties, device_selector& dev_sel, error_handler& handler = default_error);
+	context(const cl_context_properties* properties, VECTOR_CLASS<device> target_devices, error_handler& handler = default_error);
+	context(const cl_context_properties* properties, device target_device, error_handler& handler = default_error);
+
+	// TODO: Error handling via context_notify&
+	context(context_notify& handler);
+	context(cl_context c, context_notify& handler);
+	context(device_selector& dev_sel, context_notify& handler);
+	context(const cl_context_properties* properties, device_selector& dev_sel, context_notify& handler);
+	context(const cl_context_properties* properties, VECTOR_CLASS<device> target_devices, context_notify& handler);
+	context(const cl_context_properties* properties, device target_device, context_notify& handler);
+
 	~context() {}
 
 	cl_context get();
@@ -44,14 +59,6 @@ public:
 		handler.handle(error_code);
 		return param_value;
 	}
-};
-
-// Used as the notification function for contexts.
-class context_notify {
-public:
-	virtual ~context_notify() {}
-	virtual void operator()(const STRING_CLASS errinfo, const void* private_info, size_t cb) = 0;
-	virtual void operator()(program t_program) = 0;
 };
 
 } // namespace sycl
