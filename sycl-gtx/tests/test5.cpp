c41a47d70d5cb871d6dc56a5d17b2814a04e14f3
Peter Žužek <peterzuzek@gmail.com>
2014-11-06 20:08:17 +0100

    Updated test5 to stress test the current code.

--- sycl-gtx/tests/test5.cpp
+++ sycl-gtx/tests/test5.cpp
@@ -1,81 +1,77 @@
 #include "tests.h"
 
-#include "../implementation/specification/refc.h"
+#define DISPLAY_TRAIT(object, trait)	\
+debug(#trait ":\t") << object.get_info<trait>();
+
+class handler : public cl::sycl::error_handler {
+public:
+	virtual void report_error(cl::sycl::exception& error) override {
+		debug("OpenCL error:\t") << error.get_cl_code();
+	}
+};
 
 bool test5() {
-	static const int MAX_PLATFORMS = 10;
-	static const int BUFFER_SIZE = 1024;
-	cl_platform_id platforms[MAX_PLATFORMS];
-	cl_uint num_platforms = 0;
-	char buffer[BUFFER_SIZE];
+	debug();
+	using namespace cl::sycl;
 
-	bool success(true);
-	auto check_error = [&success](cl_int ret_val) {
-		if(ret_val != CL_SUCCESS) {
-			debug() << "OpenCL error:" << ret_val;
-			success = false;
+	debug("= Getting all platforms");
+	auto platforms = platform::get_platforms();
+	debug("platforms size:") << platforms.size();
+	int i = 0;
+	for(auto& p : platforms) {
+		debug();
+		debug("== platform") << (i++);
+		DISPLAY_TRAIT(p, CL_PLATFORM_PROFILE);
+		DISPLAY_TRAIT(p, CL_PLATFORM_VERSION);
+		DISPLAY_TRAIT(p, CL_PLATFORM_NAME);
+		DISPLAY_TRAIT(p, CL_PLATFORM_VENDOR);
+		//DISPLAY_TRAIT(p, CL_PLATFORM_EXTENSIONS);
+	}
+	debug();
+	debug();
+
+	debug("= Getting all devices");
+	std::vector<std::vector<device>> devices;
+	int j;
+	for(i = 0; i < platforms.size(); ++i) {
+		debug();
+		debug("== platform") << i;
+		devices.push_back(platforms[i].get_devices(CL_DEVICE_TYPE_ALL));
+		j = 0;
+		for(auto& d : devices[i]) {
+			debug();
+			debug("=== device") << (j++);
+			DISPLAY_TRAIT(d, CL_DEVICE_BUILT_IN_KERNELS);
+			DISPLAY_TRAIT(d, CL_DEVICE_NAME);
+			DISPLAY_TRAIT(d, CL_DEVICE_OPENCL_C_VERSION);
+			DISPLAY_TRAIT(d, CL_DEVICE_PROFILE);
+			DISPLAY_TRAIT(d, CL_DEVICE_VENDOR);
+			DISPLAY_TRAIT(d, CL_DEVICE_VERSION);
+			DISPLAY_TRAIT(d, CL_DRIVER_VERSION);
+			DISPLAY_TRAIT(d, CL_DEVICE_AVAILABLE);
+			DISPLAY_TRAIT(d, CL_DEVICE_COMPILER_AVAILABLE);
+			DISPLAY_TRAIT(d, CL_DEVICE_LINKER_AVAILABLE);
+			DISPLAY_TRAIT(d, CL_DEVICE_DOUBLE_FP_CONFIG);
+			DISPLAY_TRAIT(d, CL_DEVICE_SINGLE_FP_CONFIG);
+			DISPLAY_TRAIT(d, CL_DEVICE_PLATFORM);
+			DISPLAY_TRAIT(d, CL_DEVICE_TYPE);
+			DISPLAY_TRAIT(d, CL_DEVICE_QUEUE_PROPERTIES);
+			//DISPLAY_TRAIT(d, CL_DEVICE_EXTENSIONS);
 		}
-	};
+	}
+	debug();
+	debug();
 
-	check_error(clGetPlatformIDs(MAX_PLATFORMS, platforms, &num_platforms));
-
-	debug() << "num_platforms:" << num_platforms;
-
-	for(int i = 0; i < num_platforms; ++i) {
-		debug();
-		debug() << "platform num:\t" << i;
-		check_error(clGetPlatformInfo(platforms[i], CL_PLATFORM_PROFILE, BUFFER_SIZE, buffer, NULL));
-		debug() << "profile:\t" << buffer;
-		check_error(clGetPlatformInfo(platforms[i], CL_PLATFORM_VERSION, BUFFER_SIZE, buffer, NULL));
-		debug() << "version:\t" << buffer;
-		check_error(clGetPlatformInfo(platforms[i], CL_PLATFORM_NAME, BUFFER_SIZE, buffer, NULL));
-		debug() << "name:\t\t" << buffer;
-		check_error(clGetPlatformInfo(platforms[i], CL_PLATFORM_VENDOR, BUFFER_SIZE, buffer, NULL));
-		debug() << "vendor:\t\t" << buffer;
-		check_error(clGetPlatformInfo(platforms[i], CL_PLATFORM_EXTENSIONS, BUFFER_SIZE, buffer, NULL));
-		debug() << "extensions:\t" << buffer;
+	debug("= Trying to throw an exception");
+	handler h;
+	device dd(h);
+	devices.clear();
+	try {
+		devices.push_back(dd.get_devices(CL_DEVICE_TYPE_ALL));
+	}
+	catch(std::exception& e) {
+		debug("exception:\t") << e.what();
 	}
 
-	if(success && num_platforms > 0) {
-		debug();
-#define USE_SYCL 1
-#if USE_SYCL
-		using namespace cl::sycl;
-
-		auto platform = refc::allocate<cl_platform_id>();
-		auto platform_id = platform.get();
-		cl_uint num_devices;
-		auto device = refc::allocate<cl_device_id>();
-		auto device_id = device.get();
-		cl_int ret;
-
-		debug() << "getting platform";
-		check_error(clGetPlatformIDs(1, &platform_id, &num_platforms));
-		debug() << "getting device";
-		check_error(clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &num_devices));
-		debug() << "creating context";
-		auto context = refc::allocate(clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret), clReleaseContext);
-		check_error(ret);
-#else
-		cl_context context = NULL;
-		cl_device_id device_id = NULL;
-		cl_platform_id platform_id = NULL;
-		cl_uint num_devices;
-		cl_int ret;
-
-		debug() << "getting platform";
-		check_error(clGetPlatformIDs(1, &platform_id, &num_platforms));
-		debug() << "getting device";
-		check_error(clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &num_devices));
-		debug() << "creating context";
-		context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
-
-		debug() << "releasing context";
-		check_error(clReleaseContext(context));
-		debug() << "releasing device";
-		check_error(clReleaseDevice(device_id));
-#endif
-	}
-
-	return success;
+	return true;
 }
