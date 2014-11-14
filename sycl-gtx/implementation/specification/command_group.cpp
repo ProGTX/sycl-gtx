3783b8b9ca30f4d49ea9ee9ec2553ffb4a359d94
Peter Žužek <peterzuzek@gmail.com>
2014-11-14 21:06:29 +0100

    Implementing command group.

---
+++ sycl-gtx/implementation/specification/command_group.cpp
@@ -0,0 +1,5 @@
+#include "command_group.h"
+
+using namespace cl::sycl;
+
+command_group* command_group::last = nullptr;
--- sycl-gtx/implementation/specification/command_group.h
+++ sycl-gtx/implementation/specification/command_group.h
@@ -6,70 +6,37 @@
 #include "queue.h"
 #include "../common.h"
 #include "../debug.h"
+#include <functional>
 
 namespace cl {
 namespace sycl {
-
-namespace detail {
-
-struct command_group_interface {
-	virtual event kernel_event() {
-		debug::warning(__func__) << "Should not be called.";
-		return event();
-	}
-	virtual event start_event() {
-		debug::warning(__func__) << "Should not be called.";
-		return event();
-	}
-	virtual event complete_event() {
-		debug::warning(__func__) << "Should not be called.";
-		return event();
-	}
-};
-
-// This is the actual implementation of the command_group
-// Three classes are needed as a workaround to the inability to deduce arguments of a templated class at time of construction
-template <typename functorT>
-class command_group : public command_group_interface {
-public:
-	command_group(queue q, functorT functor) {
-		DSELF() << "not implemented";
-	}
-	virtual event kernel_event() override {
-		DSELF() << "not implemented";
-		return event();
-	}
-	virtual event start_event() override {
-		DSELF() << "not implemented";
-		return event();
-	}
-	virtual event complete_event() override {
-		DSELF() << "not implemented";
-		return event();
-	}
-};
-
-} // namespace detail
-
 
 // A command group in SYCL as it is defined in 2.3.1 includes a kernel to be enqueued along with all the commands
 // for queued data transfers that it needs in order for its execution to be successful.
 class command_group {
 private:
-	detail::command_group_interface group;
+	// TODO: Should be thread_local
+	static command_group* last;
 public:
 	// typename functorT: kernel functor or lambda function
 	template <typename functorT>
-	command_group(queue q, functorT functor)
-		: group(detail::command_group<functorT>(q, functor)) {}
+	command_group(queue q, functorT functor) {
+		last = this;
+		functor();
+		DSELF() << "not implemented";
+		last = nullptr;
+	}
 	event kernel_event() {
-		group.kernel_event();
+		DSELF() << "not implemented";
+		return event();
 	}
 	event start_event() {
-		group.start_event();
+		DSELF() << "not implemented";
+		return event();
 	}
 	event complete_event() {
-		group.complete_event();
+		DSELF() << "not implemented";
+		return event();
 	}
 };
 
