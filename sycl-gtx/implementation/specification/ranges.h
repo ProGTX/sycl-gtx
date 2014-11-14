24f905e9be0756782e71d851a40c64c9db0213e4
Peter Žužek <peterzuzek@gmail.com>
2014-11-14 15:48:47 +0100

    A few additions to ranges.

--- sycl-gtx/implementation/specification/ranges.h
+++ sycl-gtx/implementation/specification/ranges.h
@@ -2,12 +2,17 @@
 
 // 3.4.1 Ranges and identifiers
 
+#include "../common.h"
 #include "../debug.h"
 
 namespace cl {
 namespace sycl {
 
 namespace detail {
+
+// Forward declaration
+template <typename DataType, int dimensions>
+class buffer;
 
 template <int dimensions>
 struct range_ {
@@ -38,14 +43,14 @@ struct range<1> : protected detail::range_<1> {
 		: detail::range_<1>(size, 1, 1) {}
 };
 template <>
-struct range<2> : protected detail::range_<2> {
+struct range<2> : protected detail::range_<2>{
 	range(size_t sizeX, size_t sizeY)
 		: detail::range_<2>(sizeX, sizeY, 1) {}
 	range(size_t size[2])
 		: range(size[0], size[1]) {}
 };
 template <>
-struct range<3> : protected detail::range_<3> {
+struct range<3> : protected detail::range_<3>{
 	range(size_t sizeX, size_t sizeY, size_t sizeZ)
 		: detail::range_<3>(sizeX, sizeY, sizeZ) {}
 	range(size_t size[3])
@@ -54,22 +59,27 @@ struct range<3> : protected detail::range_<3> {
 
 template <int dimensions = 1>
 class id {
+private:
+	range<dimensions> global_size;
+	range<dimensions> local_size;
 public:
-	id(range<dimensions> global_size, range<dimensions> local_size);
+	id(
+		range<dimensions> global_size = range<dimensions>(VECTOR_CLASS<size_t>(dimensions, 0).data()),
+		range<dimensions> local_size = range<dimensions>(VECTOR_CLASS<size_t>(dimensions, 0).data())
+	)	: global_size(global_size), local_size(local_size) {}
 	int get(int dimension);
 };
 
-template <int dimensions>
+template <int dimensions = 1>
 class nd_range {
 private:
 	range<dimensions> global_size;
 	range<dimensions> local_size;
+	id<dimensions> offset;
 
 public:
-	nd_range(range<dimensions> global_size, range<dimensions> local_size)
-		: global_size(global_size), local_size(local_size) {}
-
-	nd_range(range<dimensions> global_size, range<dimensions> local_size, id offset);
+	nd_range(range<dimensions> global_size, range<dimensions> local_size, id<dimensions> offset = id<dimensions>())
+		: global_size(global_size), local_size(local_size), offset(offset) {}
 
 	range<dimensions> get_global_range() {
 		return global_size;
