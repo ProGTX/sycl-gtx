9b4645c9fd6081b207eed8066f18eb2a1e51f344
Peter Žužek <peterzuzek@gmail.com>
2014-11-14 12:00:20 +0100

    Test6 should really not be committed ...

--- sycl-gtx/tests/test6.cpp
+++ sycl-gtx/tests/test6.cpp
@@ -3,19 +3,5 @@
 bool test6() {
 	using namespace cl::sycl;
 
-	cl::sycl::queue q;
-
-	auto a = cl::sycl::exception(&q, 0);
-	auto q2 = a.get_queue();
-	auto img = a.get_image();
-
-	debug("&q\t") << &q;
-	debug("q2\t") << q2;
-	debug("img\t") << img;
-
-	int bb[5];
-	cl::sycl::buffer<int, 3> b(bb, 4);
-	auto a2 = cl::sycl::exception(&b, 0);
-
 	return true;
 }
