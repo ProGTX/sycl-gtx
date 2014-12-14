4d9d93b6ae51eb1bca800c3adf723a81202d1b94
Peter Žužek <peterzuzek@gmail.com>
2014-12-14 17:14:14 +0100

    Updated range class with arithmetic operators, added method get(), changed int to size_t.

--- sycl-gtx/implementation/specification/ranges.h
+++ sycl-gtx/implementation/specification/ranges.h
@@ -1,6 +1,6 @@
 #pragma once
 
-// 3.4.1 Ranges and identifiers
+// 3.7.1 Ranges and identifiers
 
 #include "../common.h"
 #include "../debug.h"
@@ -14,10 +14,13 @@ namespace detail {
 template <typename DataType, int dimensions>
 class buffer;
 
+
+// 3.7.1.1 Range class
+
 template <int dimensions>
 struct range_ {
 protected:
-	int dims[3];
+	size_t dims[3];
 
 	range_(size_t first, size_t second, size_t third)
 #if MSVC_LOW
@@ -33,12 +36,24 @@ protected:
 	range_(const range_&) = default;
 
 public:
-	int& operator[](size_t n) {
+	size_t& operator[](size_t n) {
 		return dims[n];
 	}
-	int operator[](size_t n) const {
+	size_t operator[](size_t n) const {
 		return dims[n];
 	}
+
+	// Return the value of the specified dimension of the range.
+	// TODO: Operator[] should take care of this
+	size_t get(int dimension) const {
+		return dims[n];
+	}
+
+protected:
+	//range_ operator*(range_ b);
+	range_ operator/(range_ divisor);
+	range_ operator+(range_ b);
+	range_ operator-(range_ b);
 };
 
 } // namespace detail
@@ -46,12 +61,28 @@ public:
 template <int dimensions = 1>
 class range;
 
+#define sycl_range_op(op, dimensions)				\
+	range operator op(range b) {					\
+		range c((const range&)(*this));				\
+		for(::size_t i = 0; i < dimensions; ++i) {	\
+			c.dims[i] op= b.dims[i];				\
+		}											\
+		return c;									\
+	}
+
+#define sycl_range_operators(dimensions)	\
+	sycl_range_op(*, dimensions)			\
+	sycl_range_op(/, dimensions)			\
+	sycl_range_op(+, dimensions)			\
+	sycl_range_op(-, dimensions)
+
 template <>
 struct range<1> : detail::range_<1> {
 	range(size_t size)
 		: detail::range_<1>(size, 1, 1) {}
 	range(size_t size[1])
 		: range(size[0]) {}
+	sycl_range_operators(1);
 };
 template <>
 struct range<2> : detail::range_<2>{
@@ -59,6 +90,7 @@ struct range<2> : detail::range_<2>{
 		: detail::range_<2>(sizeX, sizeY, 1) {}
 	range(size_t size[2])
 		: range(size[0], size[1]) {}
+	sycl_range_operators(2);
 };
 template <>
 struct range<3> : detail::range_<3>{
@@ -66,7 +98,20 @@ struct range<3> : detail::range_<3>{
 		: detail::range_<3>(sizeX, sizeY, sizeZ) {}
 	range(size_t size[3])
 		: range(size[0], size[1], size[2]) {}
+	sycl_range_operators(3);
 };
+
+#define sycl_static_range_ops(op)						\
+template <int dims>										\
+range<dims> operator op(range<dims> a, range<dims> b) {	\
+	return a op b;										\
+}
+
+sycl_static_range_ops(*);
+sycl_static_range_ops(/);
+sycl_static_range_ops(+);
+sycl_static_range_ops(-);
+
 
 template <int dimensions = 1>
 class id {
@@ -114,3 +159,7 @@ public:
 
 } // namespace sycl
 } // namespace cl
+
+#undef sycl_range_op
+#undef sycl_range_operators
+#undef sycl_static_range_ops
