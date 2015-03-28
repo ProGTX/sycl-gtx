d0abe308b0bf7e51ffb36dae047ae038d50fb4f5
Peter Žužek <peterzuzek@gmail.com>
2015-03-28 12:20:14 +0100

    Slightly revised test1 and milestones.

--- sycl-gtx/tests/test1.cpp
+++ sycl-gtx/tests/test1.cpp
@@ -5,7 +5,7 @@
 // Example from http://www.codeplay.com/portal/sycl-tutorial-1-the-vector-addition
 // (slightly modified)
 
-#define TOL (0.001)		// tolerance used in floating point comparisons
+#define TOL (0.001)		// Tolerance used in floating point comparisons
 #define LENGTH (1024)	// Length of vectors a, b and c
 
 bool test1() {
@@ -15,6 +15,7 @@ bool test1() {
 	std::vector<int> h_b(LENGTH);				// b vector
 	std::vector<int> h_c(LENGTH);				// c vector
 	std::vector<int> h_r(LENGTH, 0xdeadbeef);	// d vector (result)
+
 	// Fill vectors a and b with random float values
 	int count = LENGTH;
 	for(int i = 0; i < count; i++) {
@@ -22,6 +23,7 @@ bool test1() {
 		h_b[i] = (int)(rand() / (float)RAND_MAX);
 		h_c[i] = (int)(rand() / (float)RAND_MAX);
 	}
+
 	{
 		// Device buffers
 		buffer<int> d_a(h_a);
@@ -42,22 +44,23 @@ bool test1() {
 			});
 		});
 	}
+
 	// Test the results
 	int correct = 0;
 	float tmp;
 	for(int i = 0; i < count; i++) {
 		tmp = h_a[i] + h_b[i] + h_c[i];	// assign element i of a+b+c to tmp
 		tmp -= h_r[i];					// compute deviation of expected and output result
-		if(tmp * tmp < TOL * TOL)		// correct if square deviation is less than tolerance squared
-		{
+		if(tmp * tmp < TOL * TOL) {		// correct if square deviation is less than tolerance squared
 			correct++;
 		}
 		else {
 			//printf(" tmp %f h_a %f h_b %f h_c %f h_r %f \n", tmp, h_a[i], h_b[i], h_c[i], h_r[i]);
 		}
 	}
-	// summarize results
-	printf("R = A+B+C: %d out of %d results were correct.\n", correct, count);
+
+	// Summarize results
+	debug() << "R = A+B+C:" << correct << "out of" << count << "results were correct.";
 
 	return (correct == count);
 }
--- sycl-gtx/tests/tests.h
+++ sycl-gtx/tests/tests.h
@@ -3,11 +3,11 @@
 #include <sycl.hpp>
 #include "../implementation/debug.h"
 
-bool test1();
+bool test1();	// Milestone 3
 bool test2();	// Milestone 1
 bool test3();
 bool test4();
 bool test5();
 bool test6();
-bool test7();	// Milestone 3
+bool test7();	// Milestone 4
 bool test8();	// Milestone 2
