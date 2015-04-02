483d134af6a4ffb6c62350a555a153d36a6e76cd
Peter Žužek <peterzuzek@gmail.com>
2015-04-02 11:33:01 +0200

    Accessor for 2D index.

--- sycl-gtx/implementation/specification/accessor/buffer.h
+++ sycl-gtx/implementation/specification/accessor/buffer.h
@@ -29,6 +29,10 @@ protected:
 	cl_mem get_buffer_object() const {
 		return buf->device_data.get();
 	}
+
+	size_t access_range(int n) const {
+		return buf->rang[n];
+	}
 };
 
 SYCL_ACCESSOR_CLASS(
@@ -65,17 +69,15 @@ public:
 	}
 
 	detail::data_ref operator[](id<1> index) const {
-		using namespace detail::kernel_;
-		source::register_resource(*this);
+		detail::kernel_::source::register_resource(*this);
 		return detail::data_ref(
 			get_resource_name() + "[" + index[0].name + "]"
 		);
 	}
 	detail::data_ref operator[](id<2> index) const {
-		using namespace detail::kernel_;
-		source::register_resource(*this);
+		detail::kernel_::source::register_resource(*this);
 		return detail::data_ref(
-			get_resource_name() + "[" + index[0].name + "]"
+			get_resource_name() + "[" + index[1].name + "*" + std::to_string(access_range(0)) + " + " + index[0].name + "]"
 		);
 	}
 
