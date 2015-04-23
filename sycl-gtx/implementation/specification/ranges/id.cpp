7bf4dd851aa1f45e993b6ffdf7b7360e4b19d368
Peter Žužek <peterzuzek@gmail.com>
2015-04-23 12:09:24 +0200

    Macro consistency in the id class subscript operator.

--- sycl-gtx/implementation/specification/ranges/id.cpp
+++ sycl-gtx/implementation/specification/ranges/id.cpp
@@ -3,12 +3,14 @@
 
 using namespace cl::sycl;
 
-#define SUBSCRIPT_OPERATOR(dimension)							\
+#define SYCL_ID_SUBSCRIPT_OP(dimension)							\
 template <>														\
 detail::id_ref detail::id_<dimension>::operator[](size_t n) {	\
 	return id_ref(n, &values[n]);								\
 }
 
-SUBSCRIPT_OPERATOR(1)
-SUBSCRIPT_OPERATOR(2)
-SUBSCRIPT_OPERATOR(3)
+SYCL_ID_SUBSCRIPT_OP(1)
+SYCL_ID_SUBSCRIPT_OP(2)
+SYCL_ID_SUBSCRIPT_OP(3)
+
+#undef SYCL_ID_SUBSCRIPT_OP
