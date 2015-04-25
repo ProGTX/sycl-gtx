4782d085a8b26fe2e4a11c5fa2e0ef7e9a70e74a
Peter Žužek <peterzuzek@gmail.com>
2015-04-25 08:50:22 +0200

    Generalized 1D id operators.

--- sycl-gtx/implementation/specification/ranges/id.cpp
+++ sycl-gtx/implementation/specification/ranges/id.cpp
@@ -15,8 +15,12 @@ SYCL_ID_SUBSCRIPT_OP(3)
 
 #undef SYCL_ID_SUBSCRIPT_OP
 
+#define SYCL_ID_OPERATOR(op)										\
+	template <>														\
+	detail::data_ref detail::id_<1>::operator op(size_t n) const {	\
+		return id_ref(0, nullptr) op n;								\
+	}
 
-template <>
-detail::data_ref detail::id_<1>::operator+(size_t n) const {
-	return id_ref(0, nullptr) + n;
-}
+SYCL_ID_OPERATOR(+)
+
+#undef SYCL_ID_OPERATOR
