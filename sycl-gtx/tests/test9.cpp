464d654b2730d923198e388cce094a89caa02401
Peter Žužek <peterzuzek@gmail.com>
2015-05-16 14:19:58 +0200

    Prepared test9 (work efficient prefix sum).

---
+++ sycl-gtx/tests/test9.cpp
@@ -0,0 +1,7 @@
+#include "tests.h"
+
+// Work efficient prefix sum
+
+bool test9() {
+	return true;
+}
--- sycl-gtx/tests/tests.h
+++ sycl-gtx/tests/tests.h
@@ -5,9 +5,10 @@
 
 bool test1();	// Milestone 3
 bool test2();	// Milestone 1
-bool test3();
-bool test4();
-bool test5();
-bool test6();
+bool test3();	// Milestone 5
+bool test4();	// Milestone 6
+bool test5();	// Milestone 6
+bool test6();	// Milestone 7
 bool test7();	// Milestone 4
 bool test8();	// Milestone 2
+bool test9();
