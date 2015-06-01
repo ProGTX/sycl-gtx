2d2ef286b9ad4bd10ba73dd05a3b4762e00be65d
Peter Žužek <peterzuzek@gmail.com>
2015-06-01 19:55:42 +0200

    Exception catching on tests.

--- sycl-gtx/main.cpp
+++ sycl-gtx/main.cpp
@@ -10,21 +10,32 @@ int main() {
 	debug() << "SYCL Provisional Specification.";
 	
 	std::map<std::string, bool(*)()> tests{
-		//{ "test1", test1 },
-		//{ "test2", test2 },
-		//{ "test3", test3 },
-		//{ "test4", test4 },
-		//{ "test5", test5 },
-		//{ "test6", test6 },
-		//{ "test7", test7 },
-		//{ "test8", test8 },
+		{ "test1", test1 },
+		{ "test2", test2 },
+		{ "test3", test3 },
+		{ "test4", test4 },
+		{ "test5", test5 },
+		{ "test6", test6 },
+		{ "test7", test7 },
+		{ "test8", test8 },
+		{ "test9", test9 },
 	};
+
+	bool result = false;
 
 	for(auto&& test : tests) {
 		debug();
 		debug() << "starting" << test.first;
 		debug();
-		auto result(test.second());
+		try {
+			result = test.second();
+		}
+		catch(cl::sycl::exception& e) {
+			debug() << "cl::sycl::exception while testing" << test.first;
+		}
+		catch(std::exception& e) {
+			debug() << "std::exception while testing" << test.first;
+		}
 		debug();
 		debug() << test.first << (result ? "successful" : "failed");
 	}
