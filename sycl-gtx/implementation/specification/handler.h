39d670e4b6bf0b818f859cb8f6b8716d131ac36b
Peter Žužek <peterzuzek@gmail.com>
2015-06-12 18:55:40 +0200

    Moved function traits into separate header.

--- sycl-gtx/implementation/specification/handler.h
+++ sycl-gtx/implementation/specification/handler.h
@@ -1,48 +1,17 @@
 #pragma once
 
+// 3.5.3 SYCL functions for invoking kernels
+
 #include "access.h"
 #include "kernel.h"
 #include "ranges.h"
 #include "../common.h"
+#include "../function_traits.h"
 #include "../gen_source.h"
 #include "../invoke_source.h"
 
-// 3.5.3 SYCL functions for invoking kernels
-
 namespace cl {
 namespace sycl {
-
-namespace detail {
-
-// Used to determine first argument in a functor
-// https://functionalcpp.wordpress.com/2013/08/05/function-traits/
-template <class T>
-struct first_arg;
-
-// First function argument
-template <class R, class... Args>
-struct first_arg<R(Args...)> {
-	using type = typename std::tuple_element<0, std::tuple<Args...>>::type;
-};
-
-// Member function pointer
-template <class C, class R, class... Args>
-struct first_arg<R(C::*)(Args...)> : public first_arg<R(Args...)>
-{};
-
-// const member function pointer
-template <class C, class R, class... Args>
-struct first_arg<R(C::*)(Args...) const> : public first_arg<R(Args...)>
-{};
-
-// Functor
-template <class F>
-struct first_arg {
-	using type = typename first_arg<decltype(&F::operator())>::type;
-};
-
-} // namespace detail
-
 
 // 3.5.3.4 Command group handler class
 class handler {
