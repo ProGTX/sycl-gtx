4d0bac39c02629018542ce63a8627ab0d8956d5c
Peter Žužek <peterzuzek@gmail.com>
2015-10-09 20:13:08 +0200

    Prototype ptr_or_val class for storing either a pointer or a numeric value in one place and accessing it the same way.

--- sycl-gtx/implementation/point_ref.h
+++ sycl-gtx/implementation/point_ref.h
@@ -7,6 +7,49 @@ namespace cl {
 namespace sycl {
 namespace detail {
 
+template <typename T>
+struct ptr_or_val {
+private:
+	bool is_owner;
+	T* data;
+
+public:
+	ptr_or_val(nullptr_t, T value)
+		: is_owner(true), data(reinterpret_cast<T*>(value)) {}
+	ptr_or_val(T* ptr)
+		: is_owner(false), data(ptr) {}
+
+	operator T() const {
+		if(is_owner) {
+			return reinterpret_cast<T>(data);
+		}
+		else {
+			return *data;
+		}
+	}
+	operator T&() {
+		if(is_owner) {
+			return reinterpret_cast<T&>(data);
+		}
+		else {
+			return *data;
+		}
+	}
+
+	ptr_or_val& operator=(T n) {
+		if(is_owner) {
+			data = reinterpret_cast<T*>(n);
+		}
+		else {
+			*data = n;
+		}
+		return *this;
+	}
+	ptr_or_val<T*> operator&() {
+		return ptr_or_val<T*>(&data);
+	}
+};
+
 template <bool is_const>
 struct shared_data_base {
 	using uint_ptr = typename std::conditional<is_const, size_t* const, size_t*>::type;
