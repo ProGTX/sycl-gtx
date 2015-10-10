bf4b8c7acc133f286571f3fd7bc7d3bb0216ca4d
Peter Žužek <peterzuzek@gmail.com>
2015-10-10 08:58:18 +0200

    Moved ptr_or_val to separate file.

--- sycl-gtx/implementation/point_ref.h
+++ sycl-gtx/implementation/point_ref.h
@@ -1,58 +1,12 @@
 #pragma once
 
-#include "data_ref.h"
 #include "common.h"
+#include "data_ref.h"
+#include "ptr_or_val.h"
 
 namespace cl {
 namespace sycl {
 namespace detail {
-
-template <typename T>
-struct ptr_or_val {
-private:
-	bool is_owner;
-	T* data;
-
-public:
-	ptr_or_val(nullptr_t, T value)
-		: is_owner(true), data(reinterpret_cast<T*>(value)) {}
-	ptr_or_val(T* ptr)
-		: is_owner(false), data(ptr) {}
-
-	ptr_or_val& operator=(T n) {
-		if(is_owner) {
-			data = reinterpret_cast<T*>(n);
-		}
-		else {
-			*data = n;
-		}
-		return *this;
-	}
-
-	operator T() const {
-		if(is_owner) {
-			return reinterpret_cast<T>(data);
-		}
-		else {
-			return *data;
-		}
-	}
-	operator T&() {
-		if(is_owner) {
-			return reinterpret_cast<T&>(data);
-		}
-		else {
-			return *data;
-		}
-	}
-
-	ptr_or_val<T*> operator&() {
-		return ptr_or_val<T*>(&data);
-	}
-	ptr_or_val<typename std::remove_pointer<T>::type> operator*() {
-		return ptr_or_val<typename std::remove_pointer<T>::type>(*data);
-	}
-};
 
 template <bool is_const, typename data_basic_t = size_t>
 struct point_ref : data_ref {
