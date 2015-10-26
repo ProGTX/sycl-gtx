a3878f8c97d3ef5ffa5b2c294f2e313fd04ea71c
Peter Žužek <peterzuzek@gmail.com>
2015-10-26 20:57:47 +0100

    Started implementation of synchronizer.

---
+++ sycl-gtx/implementation/synchronizer.cpp
@@ -0,0 +1,1 @@
+#include "synchronizer.h"
---
+++ sycl-gtx/implementation/synchronizer.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "common.h"
+
+namespace cl {
+namespace sycl {
+namespace detail {
+
+} // namespace detail
+} // namespace sycl
+} // namespace cl
