6274a89d55147797ef486c80d3c14b7b3c4c7791
Peter Žužek <peterzuzek@gmail.com>
2015-10-27 22:48:58 +0100

    Updated synchronizer with host accessor support.

--- sycl-gtx/implementation/synchronizer.cpp
+++ sycl-gtx/implementation/synchronizer.cpp
@@ -1,5 +1,6 @@
 #include "synchronizer.h"
 
+#include "specification/accessor.h"
 #include "specification/buffer_base.h"
 #include "specification/queue.h"
 
@@ -7,6 +8,23 @@ using namespace cl::sycl;
 using namespace detail;
 
 std::set<queue*> synchronizer::queues;
+std::unordered_map<accessor_base*, buffer_base*> synchronizer::host_accessors;
+
+void synchronizer::wait_on_queues(buffer_base* buf) {
+	for(auto&& q : queues) {
+		if(q->buffers_in_use.count(buf) > 0) {
+			q->wait();
+		}
+	}
+}
+
+void synchronizer::flush_queues(buffer_base* buf) {
+	for(auto&& q : queues) {
+		if(q->buffers_in_use.count(buf) > 0) {
+			q->flush();
+		}
+	}
+}
 
 void synchronizer::add(queue* q) {
 	queues.insert(q);
@@ -16,10 +34,36 @@ void synchronizer::remove(queue* q) {
 	queues.erase(q);
 }
 
-void synchronizer::barrier(buffer_base* buf) {
-	for(auto&& q : queues) {
-		if(q->buffers_in_use.count(buf) > 0) {
-			q->wait();
+void synchronizer::add(accessor_base* acc, buffer_base* buf) {
+	DSELF() << acc << buf;
+	host_accessors.emplace(acc, buf);
+	wait_on_queues(buf);
+}
+
+void synchronizer::remove(accessor_base* acc, buffer_base* buf) {
+	host_accessors.erase(acc);
+	flush_queues(buf);
+}
+
+bool synchronizer::can_flush(const std::set<detail::buffer_base*>& buffers_in_use) {
+	{
+		auto d = DSELF();
+		d << "buffers_in_use";
+		for(auto& buf : buffers_in_use) {
+			d << buf;
 		}
 	}
+	{
+		auto d = DSELF();
+		d << "host_accessors";
+		for(auto&& acc : host_accessors) {
+			d << "{" << acc.first << acc.second << "}";
+		}
+	}
+	for(auto&& acc : host_accessors) {
+		if(buffers_in_use.count(acc.second) > 0) {
+			return false;
+		}
+	}
+	return true;
 }
--- sycl-gtx/implementation/synchronizer.h
+++ sycl-gtx/implementation/synchronizer.h
@@ -2,6 +2,7 @@
 
 #include "common.h"
 #include <set>
+#include <unordered_map>
 
 namespace cl {
 namespace sycl {
@@ -11,17 +12,25 @@ class queue;
 
 namespace detail {
 
-// Forward declaration
+// Forward declarations
+class accessor_base;
 class buffer_base;
 
 class synchronizer {
 private:
 	static std::set<queue*> queues;
+	static std::unordered_map<accessor_base*, buffer_base*> host_accessors;
+
+	static void wait_on_queues(buffer_base* buf);
+	static void flush_queues(buffer_base* buf);
 
 public:
 	static void add(queue* q);
 	static void remove(queue* q);
-	static void barrier(buffer_base* buf);
+	static void add(accessor_base* acc, buffer_base* buf);
+	static void remove(accessor_base* acc, buffer_base* buf);
+
+	static bool can_flush(const std::set<detail::buffer_base*>& buffers_in_use);
 };
 
 } // namespace detail
