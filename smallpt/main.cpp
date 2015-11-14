5abf62135dbe77caa3ff6c42fe0ebd9eeb722f33
Peter Žužek <peterzuzek@gmail.com>
2015-11-14 19:06:43 +0100

    Timing tests.

--- smallpt/main.cpp
+++ smallpt/main.cpp
@@ -1,9 +1,10 @@
 #include "classes.h"
 #include "msvc.h"
 
+#include <chrono>
+#include <iostream>
 #include <string>
 #include <vector>
-#include <Windows.h>
 
 extern void compute_org(int w, int h, int samps, Ray& cam, Vec& cx, Vec& cy, Vec& r, Vec* c);
 extern void compute_sycl_gtx(int w, int h, int samps, Ray& cam, Vec& cx, Vec& cy, Vec& r, Vec* c);
@@ -16,12 +17,21 @@ inline int toInt(double x) {
 }
 
 void to_file(int w, int h, Vec* c, std::string filename) {
-	FILE *f = fopen(filename.c_str(), "w");         // Write image to PPM file.
+	FILE* f = fopen(filename.c_str(), "w");         // Write image to PPM file.
 	fprintf(f, "P3\n%d %d\n%d\n", w, h, 255);
 	for(int i = 0; i < w*h; i++) {
 		fprintf(f, "%d %d %d\n", toInt(c[i].x), toInt(c[i].y), toInt(c[i].z));
 	}
 }
+
+using time_point = std::chrono::high_resolution_clock::time_point;
+
+auto now = []() {
+	return std::chrono::high_resolution_clock::now();
+};
+auto duration = [](time_point before, time_point after) {
+	return std::chrono::duration_cast<std::chrono::microseconds>(after - before).count();
+};
 
 int main(int argc, char *argv[]) {
 	int w = 1024, h = 768, samps = argc == 2 ? atoi(argv[1]) / 4 : 1; // # samples
@@ -31,13 +41,25 @@ int main(int argc, char *argv[]) {
 	std::vector<Vec> vectors_org(w*h, 0);
 	auto vector_sycl_gtx(vectors_org);
 
+	int iterations = 1;
+
 	ns_erand::reset();
-	compute_org(w, h, samps, cam, cx, cy, r, vectors_org.data());
+	auto start = now();
+	for(int i = 0; i < iterations; ++i) {
+		compute_org(w, h, samps, cam, cx, cy, r, vectors_org.data());
+	}
+	std::cout << "\n" << (duration(start, now()) / (float)iterations) << std::endl;
 	to_file(w, h, vectors_org.data(), "image_org.ppm");
 
 	ns_erand::reset();
-	compute_sycl_gtx(w, h, samps, cam, cx, cy, r, vector_sycl_gtx.data());
+	start = now();
+	for(int i = 0; i < iterations; ++i) {
+		compute_sycl_gtx(w, h, samps, cam, cx, cy, r, vector_sycl_gtx.data());
+	}
+	std::cout << "\n" << (duration(start, now()) / (float)iterations) << std::endl;
 	to_file(w, h, vector_sycl_gtx.data(), "image_sycl_gtx.ppm");
+
+	std::cin.get();
 
 	return 0;
 }
