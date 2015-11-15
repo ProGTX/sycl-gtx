c415b7320894589aef5d033adc7e8785fd6e12c8
Peter Žužek <peterzuzek@gmail.com>
2015-11-15 15:23:31 +0100

    Added normal types for third-party compilers.

--- sycl-gtx/sycl_gtx_flow.h
+++ sycl-gtx/sycl_gtx_flow.h
@@ -1,6 +1,34 @@
 
 #ifndef SYCL_GTX
 
+namespace cl {
+namespace sycl {
+
+#define SYCL_TYPE(type)	\
+using type##1 = type;
+
+SYCL_TYPE(bool)
+SYCL_TYPE(float)
+SYCL_TYPE(double)
+
+#undef SYCL_TYPE
+
+
+#define SYCL_TYPE(type)	\
+using type##1 = type;	\
+using u##type##1 = unsigned type;
+
+SYCL_TYPE(int)
+SYCL_TYPE(char)
+SYCL_TYPE(short)
+SYCL_TYPE(long)
+
+#undef SYCL_TYPE
+
+} // namespace sycl
+} // namespace cl
+
+
 #define SYCL_IF(condition) if(condition)
 
 #define SYCL_BEGIN {
