3dd8fd083c43fe616975c0d75aca8f0b2bb38fd3
Peter Žužek <peterzuzek@gmail.com>
2015-11-15 15:14:19 +0100

    Added sqrt as kernel function.

--- sycl-gtx/implementation/specification/functions/common.h
+++ sycl-gtx/implementation/specification/functions/common.h
@@ -8,6 +8,12 @@
 namespace cl {
 namespace sycl {
 
+template <class First>
+static detail::data_ref sqrt(First first) {
+	using detail::data_ref;
+	return data_ref(string_class("sqrt(") + data_ref::get_name(first) + ")");
+}
+
 template <class First, class Second>
 static detail::data_ref min(First first, Second second) {
 	using detail::data_ref;
