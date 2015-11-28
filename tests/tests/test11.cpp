87ca6d1b82b2086eb05ef4beeb7a2410860b1ac7
Peter Žužek <peterzuzek@gmail.com>
2015-11-28 19:46:36 +0100

    tests - automated checking for test11, fixed initial seed.

--- tests/tests/test11.cpp
+++ tests/tests/test11.cpp
@@ -7,12 +7,22 @@ using cl::sycl::uint1;
 using cl::sycl::uint2;
 
 // http://stackoverflow.com/a/16077942
-float1 getRandom(uint2& seed) {
-	static const float1 invMaxInt = 1.0f / 4294967296.0f;
-	uint1 x = seed.x * 17 + seed.y * 13123;
+template <class Float, class Uint1, class Uint2>
+Float getRandom(Uint2& seed) {
+	static const Float invMaxInt = 1.0f / 4294967296.0f;
+	Uint1 x = seed.x * 17 + seed.y * 13123;
 	seed.x = (x << 13) ^ x;
 	seed.y ^= x << 7;
-	return (float1)(x * (x * x * 15731 + 74323) + 871483) * invMaxInt;
+	return (Float)(x * (x * x * 15731 + 74323) + 871483) * invMaxInt;
+}
+
+float1 deviceRandom(uint2& seed) {
+	using namespace cl::sycl;
+	return getRandom<float1, uint1>(seed);
+}
+
+float hostRandom(cl_uint2& seed) {
+	return getRandom<float, cl_uint>(seed);
 }
 
 bool test11() {
@@ -25,26 +35,36 @@ bool test11() {
 	const int size = 4096;
 	buffer<float> numbers(size);
 
+	// TODO: Should not be zero
+	uint32_t startSeed_x = 24325;
+	uint32_t startSeed_y = 32536;
+
 	myQueue.submit([&](handler& cgh) {
 		auto n = numbers.get_access<access::discard_write>(cgh);
 
 		cgh.single_task<class generate>([=]() {
-			uint2 seed;
+			uint2 seed(startSeed_x, startSeed_y);
 			SYCL_FOR(int1 i = 0, i < size, ++i)
 			SYCL_BEGIN {
-				n[i] = getRandom(seed);
+				n[i] = deviceRandom(seed);
 			}
 			SYCL_END
 		});
 	});
 
+	float eps = 1e-3f;	// Don't need very high accuracy
 	auto n = numbers.get_access<access::read, access::host_buffer>();
+	cl_uint2 seed = { startSeed_x, startSeed_y };
 
-	// TODO: How to automatically check correctness?
+	// TODO: Better automatic testing
 	for(auto i = 0; i < size; ++i) {
-		cout << n[i] << ", ";
+		auto hostRnd = hostRandom(seed);
+		auto deviceRnd = n[i];
+		if(deviceRnd < hostRnd - eps || deviceRnd > hostRnd + eps) {
+			cout << i << " -> expected " << hostRnd << ", got " << deviceRnd << endl;
+			return false;
+		}
 	}
-	cout << endl;
 
 	return true;
 }
