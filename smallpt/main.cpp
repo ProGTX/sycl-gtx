0a1174dfd1d267a5423f40b747458c84b643cc01
Peter Žužek <peterzuzek@gmail.com>
2015-11-29 10:57:16 +0100

    smallpt - Added original version that will use single precision floating point.

--- smallpt/main.cpp
+++ smallpt/main.cpp
@@ -8,6 +8,8 @@
 
 extern void compute_org(int w, int h, int samps, Ray& cam, Vec& cx, Vec& cy, Vec r, Vec* c);
 extern void compute_org_openmp(int w, int h, int samps, Ray& cam, Vec& cx, Vec& cy, Vec r, Vec* c);
+extern void compute_org_sp(int w, int h, int samps, Ray& cam, Vec& cx, Vec& cy, Vec r, Vec* c);
+extern void compute_org_sp_openmp(int w, int h, int samps, Ray& cam, Vec& cx, Vec& cy, Vec r, Vec* c);
 extern void compute_sycl_gtx_cpu(int w, int h, int samps, Ray& cam, Vec& cx, Vec& cy, Vec r, Vec* c);
 extern void compute_sycl_gtx_gpu(int w, int h, int samps, Ray& cam, Vec& cx, Vec& cy, Vec r, Vec* c);
 
@@ -48,6 +50,8 @@ int main(int argc, char *argv[]) {
 	vector<pair<string, void(*)(int, int, int, Ray&, Vec&, Vec&, Vec, Vec*)>> tests = {
 		{ "org", compute_org },
 		{ "openmp", compute_org_openmp },
+		{ "openmp_single", compute_org_sp },
+		{ "org_single", compute_org_sp_openmp },
 		{ "sycl_cpu", compute_sycl_gtx_cpu },
 		{ "sycl_gpu", compute_sycl_gtx_gpu }
 	};
