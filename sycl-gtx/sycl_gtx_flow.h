550d695f74c9c79e5a84a3223f04e005b3badc66
Peter Žužek <peterzuzek@gmail.com>
2015-11-30 10:50:08 +0100

    Macros for third-party compilers should also begin automatically

--- sycl-gtx/sycl_gtx_flow.h
+++ sycl-gtx/sycl_gtx_flow.h
@@ -52,16 +52,16 @@ static auto min(First first, Second second) -> decltype(::std::min(first, second
 } // namespace cl
 
 
-#define SYCL_IF(condition) if(condition)
-
 #define SYCL_BEGIN {
 #define SYCL_END }
 
-#define SYCL_ELSE else
-#define SYCL_ELSE_IF(condition)	else if(condition)
+#define SYCL_IF(condition) if(condition) {
 
-#define SYCL_WHILE(condition) while(condition)
-#define SYCL_FOR(init, condition, increment) for(init; condition; increment)
+#define SYCL_ELSE else {
+#define SYCL_ELSE_IF(condition)	} else if(condition) {
+
+#define SYCL_WHILE(condition) while(condition) {
+#define SYCL_FOR(init, condition, increment) for(init; condition; increment) {
 
 #define SYCL_BREAK break;
 #define SYCL_CONTINUE continue;
