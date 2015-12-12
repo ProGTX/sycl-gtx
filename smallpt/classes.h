0f80e611b0e55d7d3c732d07a7f0372e7eb7c179
Peter Žužek <peterzuzek@gmail.com>
2015-12-12 18:39:56 +0100

    smallpt - modified common classes to accept templated basic type

--- smallpt/classes.h
+++ smallpt/classes.h
@@ -7,29 +7,36 @@
 
 #include <math.h>
 
-struct Vec {
-  double x, y, z;
-  Vec(double x_=0, double y_=0, double z_=0){ x=x_; y=y_; z=z_; }
-  Vec operator+(const Vec &b) const { return Vec(x+b.x,y+b.y,z+b.z); }
-  Vec operator-(const Vec &b) const { return Vec(x-b.x,y-b.y,z-b.z); }
-  Vec operator*(double b) const { return Vec(x*b,y*b,z*b); }
-  Vec mult(const Vec &b) const { return Vec(x*b.x,y*b.y,z*b.z); }
-  Vec& norm(){ return *this = *this * (1/sqrt(x*x+y*y+z*z)); }
-  double dot(const Vec &b) const { return x*b.x+y*b.y+z*b.z; } // cross:
-  Vec operator%(Vec&b){return Vec(y*b.z-z*b.y,z*b.x-x*b.z,x*b.y-y*b.x);}
+template <class type>
+struct Vec_ {
+  type x, y, z;
+  Vec_(type x_=0, type y_=0, type z_=0){ x=x_; y=y_; z=z_; }
+  Vec_ operator+(const Vec_ &b) const { return Vec_(x+b.x,y+b.y,z+b.z); }
+  Vec_ operator-(const Vec_ &b) const { return Vec_(x-b.x,y-b.y,z-b.z); }
+  Vec_ operator*(type b) const { return Vec_(x*b,y*b,z*b); }
+  Vec_ mult(const Vec_ &b) const { return Vec_(x*b.x,y*b.y,z*b.z); }
+  Vec_& norm(){ return *this = *this * (1/sqrt(x*x+y*y+z*z)); }
+  type dot(const Vec_ &b) const { return x*b.x+y*b.y+z*b.z; } // cross:
+  Vec_ operator%(Vec_&b){return Vec_(y*b.z-z*b.y,z*b.x-x*b.z,x*b.y-y*b.x);}
 };
-struct Ray { Vec o, d; Ray(Vec o_, Vec d_) : o(o_), d(d_) {} };
+template <class type>
+struct Ray_ { Vec_<type> o, d; Ray_(Vec_<type> o_, Vec_<type> d_) : o(o_), d(d_) {} };
 enum Refl_t { DIFF, SPEC, REFR };  // material types, used in radiance()
-struct Sphere {
-  double rad;       // radius
-  Vec p, e, c;      // position, emission, color
+template <class type>
+struct Sphere_ {
+  type rad;       // radius
+  Vec_<type> p, e, c;      // position, emission, color
   Refl_t refl;      // reflection type (DIFFuse, SPECular, REFRactive)
-  Sphere(double rad_, Vec p_, Vec e_, Vec c_, Refl_t refl_):
+  Sphere_(type rad_, Vec_<type> p_, Vec_<type> e_, Vec_<type> c_, Refl_t refl_):
     rad(rad_), p(p_), e(e_), c(c_), refl(refl_) {}
-  double intersect(const Ray &r) const { // returns distance, 0 if nohit
-    Vec op = p-r.o; // Solve t^2*d.d + 2*t*(o-p).d + (o-p).(o-p)-R^2 = 0
-    double t, eps=1e-4, b=op.dot(r.d), det=b*b-op.dot(op)+rad*rad;
+  type intersect(const Ray_<type> &r) const { // returns distance, 0 if nohit
+    Vec_<type> op = p-r.o; // Solve t^2*d.d + 2*t*(o-p).d + (o-p).(o-p)-R^2 = 0
+    type t, eps=(type)1e-4, b=op.dot(r.d), det=b*b-op.dot(op)+rad*rad;
     if (det<0) return 0; else det=sqrt(det);
     return (t=b-det)>eps ? t : ((t=b+det)>eps ? t : 0);
   }
 };
+
+using Vec = Vec_<double>;
+using Ray = Ray_<double>;
+using Sphere = Sphere_<double>;
