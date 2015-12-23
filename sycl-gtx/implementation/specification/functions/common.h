34d30fe5aa9303f5534bc25a9565cdfeeb6c1fec
Peter Žužek <peterzuzek@gmail.com>
2015-12-23 10:16:28 +0100

    Less copying in standard function parameters.

--- sycl-gtx/implementation/specification/functions/common.h
+++ sycl-gtx/implementation/specification/functions/common.h
@@ -8,12 +8,12 @@
 namespace cl {
 namespace sycl {
 
-#define SYCL_ONE_ARG(name)						\
-template <class First>							\
-static detail::data_ref name(First first) {		\
-	using detail::data_ref;						\
-	return	data_ref(string_class(#name "(") +	\
-			data_ref::get_name(first) + ')');	\
+#define SYCL_ONE_ARG(name)							\
+template <class First>								\
+static detail::data_ref name(const First& first) {	\
+	using detail::data_ref;							\
+	return	data_ref(string_class(#name "(") +		\
+			data_ref::get_name(first) + ')');		\
 }
 
 SYCL_ONE_ARG(cos)
@@ -24,7 +24,7 @@ SYCL_ONE_ARG(sqrt)
 #undef SYCL_ONE_ARG
 
 template <class First, class Second>
-static detail::data_ref min(First first, Second second) {
+static detail::data_ref min(const First& first, const Second& second) {
 	using detail::data_ref;
 	return data_ref(string_class("min(") + data_ref::get_name(first) + ", " + data_ref::get_name(second) + ")");
 }
