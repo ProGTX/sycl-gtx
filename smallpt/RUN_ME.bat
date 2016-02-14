774ee9f6abb5f8728cfcd3c5c34c7309ef48f418
Peter Žužek <peterzuzek@gmail.com>
2016-02-14 21:53:40 +0100

    smallpt - uncomment the first run and shorten individual testers to 5 minutes in batch file

--- smallpt/RUN_ME.bat
+++ smallpt/RUN_ME.bat
@@ -4,10 +4,10 @@ echo Tester for smallpt in SYCL
 del output.txt
 
 echo Starting sycl-gtx program, no OpenCL
-echo .\smallpt.exe 20 0 2 2>&1 | tee.exe -a output.txt
+.\smallpt.exe 5 0 2 2>&1 | tee.exe -a output.txt
 
 echo Starting sycl-gtx program, OpenCL only
-.\smallpt.exe 20 2 2>&1 | tee.exe -a output.txt
+.\smallpt.exe 5 2 2>&1 | tee.exe -a output.txt
 
 echo Starting ComputeCpp program
-.\smallpt-ccpp.exe 20 2>&1 | tee.exe -a output.txt
+.\smallpt-ccpp.exe 5 2>&1 | tee.exe -a output.txt
