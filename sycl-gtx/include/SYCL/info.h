9f1b6d8cfbfc7dbb4742f06cc8758d794e5c0518
dokipen <dokipen@dokipen.fritz.box>
2017-08-22 22:12:26 +1200

    add #ifdef __APPLE__ to make compilable on OSX

--- sycl-gtx/include/SYCL/info.h
+++ sycl-gtx/include/SYCL/info.h
@@ -3,7 +3,11 @@
 // C. Interface of Memory Object Information Descriptors
 
 #define CL_USE_DEPRECATED_OPENCL_1_2_APIS
+#ifdef __APPLE__
+#include <OpenCL/cl.h>
+#else
 #include <CL/cl.h>
+#endif
 #include <type_traits>
 
 namespace cl {
