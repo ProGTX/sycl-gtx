e66453feefe2968c1420b946157a6ae5b050640a
Peter Žužek <peterzuzek@gmail.com>
2018-05-12 23:41:48 +0100

    Run multiple clang-format processes

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -2,5 +2,7 @@
 
 repoRoot=$1
 clangFormatExe=clang-format-5.0
+numThreads=$(getconf _NPROCESSORS_ONLN)
 
-find $repoRoot -regex ".*\.\(h\|cpp\)" -exec $clangFormatExe -i {} \;
+find $repoRoot -regex ".*\.\(h\|cpp\)" \
+  | xargs -n 1 -P $numThreads $clangFormatExe -i
