ee5091f0c7d513b24acdb200417ec01043d7834a
Peter Žužek <peterzuzek@gmail.com>
2018-11-24 21:11:45 +0000

    Quote paths in scripts

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-repoRoot=${1:-.}
-clangFormatExe=${2:-clang-format-6.0}
+repoRoot="${1:-.}"
+clangFormatExe="${2:-clang-format-6.0}"
 numThreads=$(getconf _NPROCESSORS_ONLN)
 
 find $repoRoot -regex ".*\.\(h\|cpp\)" \
--- scripts/cmake-format.sh
+++ scripts/cmake-format.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-repoRoot=${1:-.}
-cmakeFormatExe=${2:-cmake-format}
+repoRoot="${1:-.}"
+cmakeFormatExe="${2:-cmake-format}"
 
 find $repoRoot -regex ".*\(CMakeLists\.txt\|\.cmake\)" \
 	       -exec $cmakeFormatExe -i {} \;
